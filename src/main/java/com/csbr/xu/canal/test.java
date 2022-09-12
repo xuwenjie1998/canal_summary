5cc257cc83cdf976a6d0b4ce98da453071e58f91
xuwenjie1998 <2995414049@qq.com>
2022-09-12 14:18:39 +0800

    测试

---
+++ src/main/java/com/csbr/xu/canal/test.java
@@ -0,0 +1,15 @@
+package com.csbr.xu.canal;
+
+import java.util.ArrayList;
+
+/**
+ * @program: canal
+ * @description: test
+ * @author: xwj
+ * @create: 2022-09-12 14:16
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
